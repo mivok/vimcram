6850cc597d25ef99da710c2779f8827a29b0c512
Mark Harrison <mark@mivok.net>
2012-04-08 21:51:25 -0400

    Allow setting debug mode from run_test.sh (refs #6)

--- run_test.sh
+++ run_test.sh
@@ -1,8 +1,29 @@
 #!/bin/bash
+
+DEBUG=
+while getopts "d" opt; do
+    case $opt in
+        d)
+            DEBUG=1
+            ;;
+    esac
+done
+shift $(($OPTIND-1))
+
 if [[ -z $1 ]]; then
-    echo "Usage: $0 [filename]"
+    echo "Usage: $0 [options] FILENAME"
     echo "Run a test using vimcram"
+    echo
+    echo "Options:"
+    echo "  -d  Enable debug mode"
     exit 1
 fi
+
 VIMOPTIONS="-N -u NONE" # Don't load user scripts or .vimrc
-vim $VIMOPTIONS -S vimcram.vim -c 'RunTest "'$1'"'
+
+VIMCOMMANDS="RunTest \"$1\""
+if [[ -n $DEBUG ]]; then
+    VIMCOMMANDS="let g:vimcram_debug=1 | $VIMCOMMANDS"
+fi
+
+vim $VIMOPTIONS -S vimcram.vim -c "$VIMCOMMANDS"
